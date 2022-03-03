cdb478158a84a97d3028a76cf4109ceca166e6fb
KingJules92 <julian.jung@online.de>
2022-03-03 07:52:41 +0100

    Update hangman.py
    Main Game working;

--- Projects/hangman.py
+++ Projects/hangman.py
@@ -1,25 +1,30 @@
 import random
 
+# Function to split a String into single Characters
 def split_word(word):
     return [char for char in word]
 
-word_list = ["bird"]
+
+word_list = ["bird", "is", "the", "word"]
+
+
 word  = random.choice(word_list)
 
-attempt = 0
-
-split = split_word(word)
+# Splitting the Word
+secret_word = split_word(word)
 
 guessed_word = ["_ " for char in word]
 
 
-for x in range(5):
+for x in range(11):
 
+    print(guessed_word)
+    print(" # "*10)
     user_input = input("Buchstaben eingeben: ")
 
     if user_input in word:
-        print("Correct")
         for i in range(len(word)):
-            if split[i] == user_input:
+            if secret_word[i] == user_input:
                 guessed_word[i] = user_input
         print(guessed_word)
+        print(" # "*10)
