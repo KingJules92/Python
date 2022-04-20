c75e157bb0d79ad2c9ea46749feadd26306d5b8e
KingJules92 <julian.jung@online.de>
2022-04-20 13:26:29 +0200

    Create user.py

---
+++ Projects/user.py
@@ -0,0 +1,25 @@
+from multiprocessing import reduction
+
+
+class User:
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self.age = age
+
+    def full_name(self):
+        return f"{self.first} {self.last}"
+    
+    def initials(self):
+        return f"{self.first[0]}.{self.last[0]}."
+    
+    def likes(self, thing):
+        return f"{self.first} likes {thing}"
+
+    def is_senior(self):
+        return self.age >= 65
+
+    def birthday(self):
+        self.age += 1
+        return f"Happy {self.age}th birthday, {self.first}"
+
