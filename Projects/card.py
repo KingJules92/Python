adcc00b8d1697a228cc737497ea9960c5038abc8
KingJules92 <julian.jung@online.de>
2022-04-25 21:39:27 +0200

    Update card.py
    initialised functions

--- Projects/card.py
+++ Projects/card.py
@@ -1,7 +1,7 @@
 import random
 
-# allowed_suite = ["Hearts", "Diamonds", "Clubs", "Spades"]
-# allowed_value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+allowed_suite = ["Hearts", "Diamonds", "Clubs", "Spades"]
+allowed_value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 class Card:
     def __init__(self, suite, value):
@@ -12,5 +12,21 @@ class Card:
         return f"{self.value} of {self.suite}"
 
 class Deck:
+
     def __init__(self):
+        suite = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        self.cards = [Card(value, suite) for value in value and suite in suite]
+
+    def __repr__(self):
         pass
+
+    def shuffle(self):
+        pass
+
+    def _deal(self):
+        pass
+
+    def deal_hand(self):
+        pass
+
